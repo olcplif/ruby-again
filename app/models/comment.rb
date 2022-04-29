6bc9a8b9df81d5861ec83652a7eb5ba0259ad1cc
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-04-29 14:00:21 +0300

    used concerns

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,5 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
-
-  VALID_STATUSES = %w[public private archived]
-
-  validates :status, inclusion: { in: VALID_STATUSES}
-
-
 end
