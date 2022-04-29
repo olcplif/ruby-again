c36bee4ad2fa4535492b5ffd31120269402f7d3c
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-04-29 14:39:46 +0300

    fixed comment's status

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
   include Visible
 
   belongs_to :article
+
+  validates :commenter, presence: true
+  validates :body, presence: true, length: {minimum: 7}
+  validates :status, presence: true
 end
